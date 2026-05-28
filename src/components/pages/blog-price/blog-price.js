9bff0bf2cb693a3721d80799cfe678c45340b8f8
Yevhen Sydorov <105035206+Sokiryanin@users.noreply.github.com>
2026-05-28 12:36:42 +0200

    add blog-price page

---
+++ src/components/pages/blog-price/blog-price.js
@@ -0,0 +1,1 @@
+import './blog-price.scss';
---
+++ src/components/wordpress/fls-theme/pages/blogprice.php
@@ -0,0 +1,9 @@
+
+<? /* Template Name: blogprice page template */ ?>
+<? get_header() ?>
+<main class="page">
+	<div data-fls-blogprice class="blogprice">
+	</div>
+</main>
+<? get_footer() ?>
+			
